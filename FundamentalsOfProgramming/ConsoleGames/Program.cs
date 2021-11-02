4632fb1981587566d6ecc72aefbab0a547748ca6
Nicholas <nick.notaweeb@gmail.com>
2021-11-02 15:14:38 +0000

    Project Creation

---
+++ FundamentalsOfProgramming/ConsoleGames/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleGames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
