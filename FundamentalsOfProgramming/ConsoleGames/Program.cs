0722e0d4ff6f22c1aa3f70566b9a266c8901520f
Nicholas <nick.notaweeb@gmail.com>
2021-11-02 15:30:00 +0000

    made map, and started render subroutine

--- FundamentalsOfProgramming/ConsoleGames/Program.cs
+++ FundamentalsOfProgramming/ConsoleGames/Program.cs
@@ -4,9 +4,29 @@ namespace ConsoleGames
 {
     class Program
     {
+        static int[,] map = new int[,]{
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+        };
+
+        static void Render()
+        {
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Console.Write("□ "); // ⬛
+                    //Console.Write(map[x, y]);
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Welcome to Noughts and Crosses");
+            Render();
         }
     }
 }
