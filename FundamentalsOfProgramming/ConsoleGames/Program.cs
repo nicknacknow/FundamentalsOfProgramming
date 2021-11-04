388a5fd81ab087cfeedb5b6676760c85c9fa058f
Nicholas <nick.notaweeb@gmail.com>
2021-11-04 11:04:13 +0000

    made it work

--- FundamentalsOfProgramming/ConsoleGames/Program.cs
+++ FundamentalsOfProgramming/ConsoleGames/Program.cs
@@ -16,9 +16,9 @@ namespace ConsoleGames
             switch (dig)
             {
                 case 1:
-                    return '⬛';
+                    return '■';
                 case 2:
-                    return '⃝';
+                    return '❌'; //'⃝
                 default:
                     return '□';
             }
@@ -40,18 +40,93 @@ namespace ConsoleGames
             }
         }
 
+        static bool Check(out int winner)
+        {
+            // horizontal check
+            for (int x = 0; x < 3; x++)
+            {
+                int current = 0;
+                bool changed = false;
+                for (int y = 0; y < 3; y++)
+                {
+                    if (changed) continue;
+                    if (y==0) current = map[x, y];
+                    if (current == 0 || (current != map[x, y])) changed = true;
+                }
+                if (!changed)
+                {
+                    winner = current;
+                    return true;
+                }
+            }
+            // vertical check
+            for (int x = 0; x < 3; x++)
+            {
+                int current = 0;
+                bool changed = false;
+                for (int y = 0; y < 3; y++)
+                {
+                    if (changed) continue;
+                    if (y == 0) current = map[y,x];
+                    if (current == 0 || (current != map[y,x])) changed = true;
+                }
+                if (!changed)
+                {
+                    winner = current;
+                    return true;
+                }
+            }
+            // diagonal check 1
+            {
+                int current = 0;
+                bool changed = false;
+                for (int x = 0; x < 3; x++)
+                {
+                    if (changed) continue;
+                    if (x == 0) current = map[x, x];
+                    if (current == 0 || (current != map[x, x])) changed = true;
+                }
+                if (!changed)
+                {
+                    winner = current;
+                    return true;
+                }
+            }
+            // diagonal check 2
+            {
+                int current = 0;
+                bool changed = false;
+                for (int x = 2; x >= 0; x--)
+                {
+                    if (changed) continue;
+                    if (x == 2) current = map[2 - x, x];
+                    if (current == 0 || (current != map[2 - x, x])) changed = true;
+                }
+                if (!changed)
+                {
+                    winner = current;
+                    return true;
+                }
+            }
+
+            winner = 0;
+            return false;
+        }
+
         static void SetToMap(int[] pos, int c)
         {
             previousSelection = map[pos[0], pos[1]];
             map[pos[0],pos[1]] = c;
         }
 
-        static bool Interact(bool b = false)
+        static bool Interact(bool enterPressed = false)
         {
             ConsoleKey key = Console.ReadKey().Key;
 
-            if (!b)
+            if (!enterPressed)
                 SetToMap(currentSelection, previousSelection);
+            //else
+            //    SetToMap(currentSelection, playerTurn);
 
             switch (key)
             {
@@ -68,13 +143,17 @@ namespace ConsoleGames
                     if (currentSelection[1] < 2) currentSelection[1]++;
                     break;
                 case ConsoleKey.Enter:
-                    if (previousSelection == 0) // need to work on Enter etc
+                    if (map[currentSelection[0], currentSelection[1]] == 0)
+                    { // need to fix this 
                         SetToMap(currentSelection, playerTurn);
-                    return true;
+                        return true;
+                    }
+                    break;
                 default:
                     break;
             }
             SetToMap(currentSelection, playerTurn);
+
             return false;
         }
 
@@ -82,16 +161,21 @@ namespace ConsoleGames
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-            bool test = false;
+            int winner;
+            bool enterPressed = false;
             while (true)
             {
                 Console.WriteLine("Welcome to Noughts and Crosses");
                 Render();
-                if (Interact(test)) test = true;
-                else test = false;
+                if (enterPressed = Interact(enterPressed))
+                {
+                    playerTurn = playerTurn == 1 ? 2 : 1;
+                    if (Check(out winner)) break;
+                }
 
                 Console.Clear();
             }
+            Console.WriteLine($"winner!!!!! {(winner == 1 ? "nought" : "cross")}");
         }
     }
 }
